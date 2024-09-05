dd2c1ee69dc11e86f991bc8cc5ac9ee5d127eed5
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2024-09-05 19:43:07 +0100

    The app file

---
+++ Python_coding/django_api/mysite/api/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import BlogPost
+from .serializers import BlogPostSerializer
+from rest_framework.views import APIView
+
+# Create your views here.
+class BlogPostListCreate(generics.ListCreateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    
+    def delete(self, request, *args, **kwargs):
+        BlogPost.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = 'pk'
+    
+    
+class BlogPostList(APIView):
+    def get(self, request, format=None):
+        title = request.query_params.get('title', "")
+        
+        if title:
+            # get the title from the query params(if none, default to empty string)
+            blog_posts = BlogPost.objects.filter(title__icontains=title)
+        else:
+            blog_posts = BlogPost.objects.all()
+    
+        serializer = BlogPostSerializer(blog_posts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
