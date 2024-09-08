bd4a272023e3a2f066f9f4b0ab425be8271151fa
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2024-09-08 13:01:07 +0100

    The modified sum

--- Python_coding/good_logics/0_sum_warmups.py
+++ Python_coding/good_logics/0_sum_warmups.py
@@ -4,9 +4,19 @@ def sum1(n):
     """
     final_sum = 0
     for x in range(n+1):
-        print(x)
         final_sum += x
         
     return final_sum
 
-sum1(5)
+
+def sum2(n):
+    """
+    Take an input of n and return the sum of the numbers from 0 to n
+    """
+    return (n*(n+1))/2
+
+
+first = sum1(5)
+second =sum2(5)
+
+print(first, second)
