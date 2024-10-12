e22d53d354b8cd81e939cfc768e4cfa709906c1b
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2024-10-12 13:40:45 +0100

    Creating a simple word replacement program

---
+++ Python_coding/small_coding_projects/word_replacement_program.py
@@ -0,0 +1,8 @@
+def replace_word():
+    str = "Hi guys I am the Nobleman, here to teach you one or two tricks"
+    word_to_replace = input("enter the word to replace: ")
+    word_replacement = input("enter the word replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+    
+    
+replace_word()
