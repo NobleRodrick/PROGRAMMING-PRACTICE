fb6223658d04b8766220fbb50aa8c7faaeae0d63
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2024-10-14 15:29:49 +0100

    Playing with the stack overflow public api, backend is really cool

---
+++ APIs_in_Python/Stack_Overflow_APIs.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+response = requests.get('https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow')
+
+for data in response.json()['items']:
+    if data['answer_count'] == 0:
+        print(data['title'])
+        print(data['link'])
+    else:
+        print('skipped')
+    print()
